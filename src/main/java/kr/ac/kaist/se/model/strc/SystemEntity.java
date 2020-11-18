9e54c770957eb5780de403b7af712139a1dba0a8
YONGJUN SHIN <yongjun9461@gmail.com>
2020-11-18 14:09:14 +0900

    update SystemEntity

--- src/main/java/kr/ac/kaist/se/model/strc/SystemEntity.java
+++ src/main/java/kr/ac/kaist/se/model/strc/SystemEntity.java
@@ -4,8 +4,7 @@ import kr.ac.kaist.se.intf.Communicatable;
 import kr.ac.kaist.se.intf.Stateful;
 import kr.ac.kaist.se.model.abst.sys._SimActionableObject_;
 
-abstract public class SystemEntity extends _SimActionableObject_
-        implements Communicatable, Stateful {
+abstract public class SystemEntity extends _SimActionableObject_ implements Communicatable, Stateful {
 
     protected EnumSystemState curState;
 }
