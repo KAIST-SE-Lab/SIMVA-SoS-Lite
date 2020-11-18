e86bce7b75cd8854dd70a89ebb46d38115b6da1e
YONGJUN SHIN <yongjun9461@gmail.com>
2020-11-18 14:10:00 +0900

    Update Task.java

--- src/main/java/kr/ac/kaist/se/model/behv/Task.java
+++ src/main/java/kr/ac/kaist/se/model/behv/Task.java
@@ -5,7 +5,6 @@ import kr.ac.kaist.se.model.abst.proc._SimProcess_;
 import java.util.ArrayList;
 
 abstract public class Task extends _SimProcess_ {
-
     protected ArrayList<Task> subTaskList;
     protected ArrayList<Role> roleList;
 
