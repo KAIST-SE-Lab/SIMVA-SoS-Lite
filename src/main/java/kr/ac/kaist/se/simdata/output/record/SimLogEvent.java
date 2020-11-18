438f8bee94b197bb7c3a622da0fa4ed0f9590267
YONGJUN SHIN <yongjun9461@gmail.com>
2020-11-18 14:10:44 +0900

    Update SimLogEvent.java

--- src/main/java/kr/ac/kaist/se/simdata/output/record/SimLogEvent.java
+++ src/main/java/kr/ac/kaist/se/simdata/output/record/SimLogEvent.java
@@ -3,7 +3,6 @@ package kr.ac.kaist.se.simdata.output.record;
 import kr.ac.kaist.se.model.evnt._SimEvent_;
 
 public class SimLogEvent extends _SimEvent_ {
-
     //TODO: timestamp
     protected String timestamp;
 }
