6bd8fc2fa006cf8bc31fd8bbb753bf6d8517b656
YONGJUN SHIN <yongjun9461@gmail.com>
2020-11-18 14:11:22 +0900

    update MsgEnum

--- src/main/java/kr/ac/kaist/se/model/abst/comm/EnumMsgType.java
+++ src/main/java/kr/ac/kaist/se/model/abst/comm/EnumMsgType.java
@@ -1,4 +1,5 @@
 package kr.ac.kaist.se.model.abst.comm;
 
 public enum EnumMsgType {
+    
 }
--- src/main/java/kr/ac/kaist/se/simdata/output/record/SimSnapshot.java
+++ src/main/java/kr/ac/kaist/se/simdata/output/record/SimSnapshot.java
@@ -1,5 +1,5 @@
 package kr.ac.kaist.se.simdata.output.record;
 
 abstract public class SimSnapshot {
-    
+
 }
